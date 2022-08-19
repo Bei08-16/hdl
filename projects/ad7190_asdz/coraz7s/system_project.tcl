9ea994637ff2880033abb730ab404a4adf665b0d
Iulia Moldovan <iulia.moldovan@analog.com>
2022-08-19 13:47:45 +0100

    ad7190_ebz->ad7190_asdz renaming
    Signed-off-by: Iulia Moldovan <iulia.moldovan@analog.com>

--- projects/ad7190_asdz/coraz7s/system_project.tcl
+++ projects/ad7190_asdz/coraz7s/system_project.tcl
@@ -2,13 +2,13 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project ad7190_ebz_coraz7s
+adi_project ad7190_asdz_coraz7s
 
-adi_project_files ad7190_ebz_coraz7s [list \
+adi_project_files ad7190_asdz_coraz7s [list \
     "$ad_hdl_dir/library/common/ad_iobuf.v" \
     "$ad_hdl_dir/projects/common/coraz7s/coraz7s_system_constr.xdc" \
     "system_constr.xdc" \
     "system_top.v"
 ]
 
-adi_project_run ad7190_ebz_coraz7s
+adi_project_run ad7190_asdz_coraz7s
